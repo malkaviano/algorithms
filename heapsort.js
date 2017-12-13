fbcf39a71acc1d57cbb8316b37d5d1115b9aa121
Malkaviano <rafael.kss@gmail.com>
2017-12-13 17:52:27 +0000

    Heapsort

---
+++ heapsort.js
@@ -0,0 +1,119 @@
+'use strict';
+
+function swap(array, x, y) {
+  [array[x], array[y]] = [array[y], array[x]];
+}
+
+module.exports = {
+  PARENT(i) {
+    if(--i < 0) {
+      i = 0;
+    }
+    
+    return Math.floor(i / 2);
+  },
+
+  LEFT(i) {
+    return 2 * (i + 1) - 1;
+  },
+
+  RIGHT(i) {
+    return 2 * (i + 1);
+  },
+
+  MAX_HEAPIFY(heap, i) {
+    let l = this.LEFT(i),
+        r = this.RIGHT(i),
+        largest = i;
+
+    if(l < heap.size && heap[l] > heap[i]) {
+      largest = l;
+    }
+    
+    if(r < heap.size && heap[r] > heap[largest]) {
+      largest = r;
+    }
+
+    if(largest !== i) {
+      swap(heap, i, largest);
+      this.MAX_HEAPIFY(heap, largest);
+    }
+  },
+
+  BUILD_MAX_HEAP(heap) {
+    heap.size = heap.length;
+
+    for(let i = Math.floor(heap.length / 2); i >= 0; --i) {
+      this.MAX_HEAPIFY(heap, i);
+    }
+  },
+
+  HEAPSORT(heap) {
+    this.BUILD_MAX_HEAP(heap);
+
+    for(let i = (heap.length - 1); i > 0; --i) {
+      swap(heap, i, 0);
+      heap.size--;
+
+      this.MAX_HEAPIFY(heap, 0);
+    }
+  },
+
+  HEAP_MAXIMUM(heap) {
+    return heap[0];
+  },
+
+  HEAP_EXTRACT_MAXIMUM(heap) {
+    if(heap.size < 1) {
+      throw new Error("Heap underflow");
+    }
+
+    const max = this.HEAP_MAXIMUM(heap);
+
+    heap[0] = heap[heap.size - 1];
+
+    heap.size--;
+
+    this.MAX_HEAPIFY(heap, 0);
+
+    return max;
+  },
+
+  HEAP_INCREASE_KEY(heap, i, inc) {
+    if(inc < 0) {
+      throw new Error('Increment is negative');
+    }
+
+    if(heap[i] === undefined) {
+      heap[i] = 0;
+    }
+
+    heap[i] += inc;
+
+    while (i >=0 && heap[this.PARENT(i)] < heap[i]) {
+      const parent = this.PARENT(i);
+
+      swap(heap, i, parent);
+
+      i = parent;
+    }
+  },
+
+  MAX_HEAP_INSERT(heap, key) {
+    heap.size++;
+
+    this.HEAP_INCREASE_KEY(heap, heap.size - 1, key);
+  },
+
+  BUILD_MAX_HEAP_INSERT(array) {
+    const heap = [];
+    
+    heap.size = 0;
+
+    for(let key of array) {
+      this.MAX_HEAP_INSERT(heap, key);
+    }
+
+    return heap;
+  },
+};
---
+++ test/heapsort.js
@@ -0,0 +1,139 @@
+'use strict';
+
+const expect = require('chai').expect,
+      heapsort = require('../heapsort');
+
+const newArray = function() {
+  return [17, 27, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0];
+};
+
+describe('PARENT', function() {
+  it('returns 0', function() {
+    expect(heapsort.PARENT(0)).to.equal(0);
+    expect(heapsort.PARENT(2)).to.equal(0);
+  });
+
+  it('returns 2', function() {
+    expect(heapsort.PARENT(5)).to.equal(2);
+  });
+});
+
+describe('LEFT', function() {
+  it('returns 1', function() {
+    expect(heapsort.LEFT(0)).to.equal(1);
+  });
+
+  it('returns 3', function() {
+    expect(heapsort.LEFT(1)).to.equal(3);
+  });
+});
+
+describe('RIGHT', function() {
+  it('returns 2', function() {
+    expect(heapsort.RIGHT(0)).to.equal(2);
+  });
+
+  it('returns 4', function() {
+    expect(heapsort.RIGHT(1)).to.equal(4);
+  });
+});
+
+describe('MAX_HEAPIFY', function() {
+  it('changes heap, max parent on index 2', function() {
+    const heap = newArray();
+    heap.size = heap.length;
+
+    heapsort.MAX_HEAPIFY(heap, 2); //[ 27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0]
+
+    expect(heap[2]).to.equal(10);
+    expect(heap[5]).to.equal(9);
+    expect(heap[12]).to.equal(3);
+  });
+});
+
+describe('BUILD_MAX_HEAP', function() {
+  it('build max priority heap', function() {
+    const heap = newArray();
+
+    heapsort.BUILD_MAX_HEAP(heap); //[ 27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0]
+  
+    expect(heap).to.deep.equal([ 27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0]);
+  });
+});
+
+describe('HEAPSORT', function() {
+  it('generated a sorted heap', function() {
+    const heap = newArray();
+
+    heapsort.HEAPSORT(heap); //[ 0, 1, 3, 4, 5, 7, 8, 9, 10, 12, 13, 16, 17, 27]
+
+    expect(heap).to.deep.equal([ 0, 1, 3, 4, 5, 7, 8, 9, 10, 12, 13, 16, 17, 27]);
+
+  });
+});
+
+describe('HEAP_MAXIMUM', function() {
+  it('returns maximum value', function() {
+    const heap = newArray();
+
+    heapsort.BUILD_MAX_HEAP(heap); //[ 27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0]
+
+    expect(heapsort.HEAP_MAXIMUM(heap)).to.equal(27);
+  });
+});
+
+describe('HEAP_EXTRACT_MAXIMUM', function() {
+  it('extracts maximum value', function() {
+    const heap = newArray();
+
+    heapsort.BUILD_MAX_HEAP(heap); //[ 27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0]
+
+    expect(heapsort.HEAP_EXTRACT_MAXIMUM(heap)).to.equal(27);
+
+    expect(heap.size).to.equal(13);
+  });
+});
+
+describe('HEAP_INCREASE_KEY', function() {
+  it('increase index 6 by 30', function() {
+    const heap = newArray();
+
+    heapsort.BUILD_MAX_HEAP(heap); //[ 27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0]
+
+    heapsort.HEAP_INCREASE_KEY(heap, 6, 30);
+
+    expect(heap[0]).to.equal(31);
+  });
+
+  it('increase index 6 by 10', function() {
+    const heap = newArray();
+
+    heapsort.BUILD_MAX_HEAP(heap); //[ 27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0]
+
+    heapsort.HEAP_INCREASE_KEY(heap, 6, 10);
+
+    expect(heap[2]).to.equal(11);
+  });
+});
+
+describe('MAX_HEAP_INSERT', function() {
+  it('insert 20 into the heap', function() {
+    const heap = newArray();
+
+    heapsort.BUILD_MAX_HEAP(heap); //[ 27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0]
+
+    heapsort.MAX_HEAP_INSERT(heap, 20);
+
+    expect(heap[2]).to.equal(20);
+  });
+});
+
+describe('BUILD_MAX_HEAP_INSERT', function() {
+  it('build max priority heap', function() {
+    const array = newArray();
+
+    const heap = heapsort.BUILD_MAX_HEAP_INSERT(array); //[ 27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 3, 8, 0]
+
+    expect(heap).to.deep.equal([ 27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 3, 8, 0]);
+  });
+});
